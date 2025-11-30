59df74b37dcfb46907645b71eed1c61907f33ed5
Tianjun7 <leovan754@gmail.com>
2025-11-30 15:03:02 +0000

    property list completed

--- components/Property-list.jsx
+++ components/Property-list.jsx
@@ -2,10 +2,9 @@ export default function Propertylist({ properties }){
     
     return (
     <div>
-        
         <ul>
         {properties.map((property) => {
-            return <li key={property}>{property}</li>
+            return <li key={property.property_id}>{property.property_name}</li>
         })}
         </ul>
     </div>
