b4b6f3e786b4fa6375f9f534eaf27e693a1ae49d
Tianjun7 <leovan754@gmail.com>
2026-01-28 22:57:50 +0000

    Sort by added

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -5,12 +5,16 @@ export default function Filter(){
         <div className="filter">
             <Toggle name="filter">
                 <form className = "filter=from">
-                    <button>cost per night</button>
                     <label htmlFor="max-price">Max price:</label>
                     <input id="max-price" type="text" />
                     <label htmlFor="min-price">Min price</label>
                     <input id="min-price" type="text" />
                 </form>
+            </Toggle>
+
+            <Toggle name="sort by">
+                <label className="Price per night(desc)" htmlFor="price per night(desc)">Price per night(Highest to Lowest)</label>
+                <label className="Price per night(asc)" htmlFor="price per night(asc)">Price per night(Lowest to Highest)</label>
             </Toggle>
         </div>
     )
