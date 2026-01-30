16d4659e1bba22f2ad7006f8ce6c9b32d3c69af5
Tianjun7 <leovan754@gmail.com>
2026-01-30 12:02:39 +0000

    Changed sort buttons

--- components/Sort.jsx
+++ components/Sort.jsx
@@ -4,8 +4,8 @@ export default function Sort(){
     return(
         <div className="Sort">
             <Toggle name="sort by">
-                <label className="Price per night(desc)" htmlFor="price per night(desc)">Price per night(Highest to Lowest)</label>
-                <label className="Price per night(asc)" htmlFor="price per night(asc)">Price per night(Lowest to Highest)</label>
+                <button className="Price per night(desc)" htmlFor="price per night(desc)">Price per night(Highest to Lowest)</button>
+                <button className="Price per night(asc)" htmlFor="price per night(asc)">Price per night(Lowest to Highest)</button>
             </Toggle>
         </div>
     )
