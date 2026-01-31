f47d20e9e89596626cb3e24b7b217caa9d944dfa
Tianjun7 <leovan754@gmail.com>
2026-01-31 23:23:39 +0000

    Added sort and filter states

--- pages/Properties.jsx
+++ pages/Properties.jsx
@@ -9,6 +9,8 @@ export default function Properties(){
     const [properties, setProperties] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [hasErrored, setHasErrored] = useState(null)
+    const [sortBy, setSortBy] = useState()
+    const [filters, setFilters] = useState()
 
     const fetchProperties = async () => {
         try{
